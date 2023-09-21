3fa476f339e9d121f80daef38ade830ef2c84a49
umarbinmusa <umarmusa20002@gmail.com>
2023-09-21 16:41:18 -0700

    changes

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,41 +3,37 @@ import { Link } from "react-router-dom";
 
 
 
-function Footer () {
-    return(
-        <div class="grid grid-cols-4 gap-30   mt-2    ">
-        <div>
-          <Link to="/">
-          <img src="./images/logo1.png" class="w-12" alt="" />
-          
-          </Link>
-        </div>
-        <div>
-          <Link to="Airtime">
-        <img src="./images/tup-up.png" class="w-12" alt="" />
-        
-        </Link>
-        </div>
-        <div>
-          <Link to="/Data">
-        <img src="./images/wifi.png" class="w-12" alt="" />
-        
-        </Link>
-        </div>
-        <div>
-          <Link to="Contact">
-        <img src="./images/logo1.png" class="w-12" alt="" />
-        
-        </Link>
-        </div>
-        
-       
-           
-          </div>
-  
+function Footer() {
+  return (
+    <div class="grid grid-cols-4 gap-30 mt-14 ml-5 space-x-5 bg-white rounded-full lg:space-x-10 lg:ml-20">
+      <div>
+        <Link to="/">
+          <img src="./images/settings.png" class="w-12 " alt="" />
 
+        </Link>
+      </div>
+      <div>
+        <Link to="Airtime">
+          <img src="./images/list.png" class="w-12" alt="" />
 
-    );
+        </Link>
+      </div>
+      <div>
+        <Link to="/Data">
+          <img src="./images/wifi.png" class="w-12" alt="" />
+
+        </Link>
+      </div>
+      <div>
+        <Link to="Contact">
+          <img src="./images/profile-user.png" class="w-12" alt="" />
+
+        </Link>
+      </div>
+
+    </div>
+
+  );
 }
 
 export default Footer;
---
+++ src/pages/land.jsx
@@ -0,0 +1,48 @@
+
+
+function land() {
+    return (
+
+        <div class="min-h-screen"
+            style={{ background: 'linear-gradient(to bottom,#F0F4FD,#A1A3BA)', }}>
+
+            <div class="flex flex-col lg:flex-row   ">
+                <div class='mt-5  ml-7 text-center lg:text-left'>
+                    <h1 class='font-black text-5xl lg:text-6xl'>
+                        WELCOME TO
+                        <br />
+                        <span class='font-quicksand font-light'>SPP-DATA-DEPOT</span>
+                    </h1>
+                    <p class='text-xl text-gray-900'>WE PROVIDE THE BEST VTU SYSTEM FOR YOU AND OUR PRODUCT ARE TESTED ABD TRUSTED BY OUR USERS</p>
+                    <p class='text-xl text-gray-900'>AND ALSO WE PROVIDE APIS TO PEOPLE WHO WANT TO USE OUR API </p>
+                    <input class="inline-block bg-indigo-500  px-10 py-5 rounded-full shadow-lg uppercase text-lg tracking-wide mt-5 ml-12" type="button" value="contact-us" />
+
+                </div>
+
+                <div > <img src="./images/svg1.svg" alt='' className='w-screen lg:max-w-xl' /></div>
+            </div>
+            <div class="flex flex-col lg:flex-row   ">
+                <div class='mt-5  ml-7 text-center lg:text-left'>
+                    <h1 class='font-black text-5xl lg:text-6xl'>
+                        WELCOME TO
+                        <br />
+                        <span class='font-quicksand font-light'>SPP-DATA-DEPOT</span>
+                    </h1>
+                    <p class='text-xl text-gray-900'>WE PROVIDE THE BEST VTU SYSTEM FOR YOU AND OUR PRODUCT ARE TESTED ABD TRUSTED BY OUR USERS</p>
+                    <p class='text-xl text-gray-900'>AND ALSO WE PROVIDE APIS TO PEOPLE WHO WANT TO USE OUR API </p>
+                    <input class="inline-block bg-indigo-500  px-10 py-5 rounded-full shadow-lg uppercase text-lg tracking-wide mt-5 ml-12" type="button" value="contact-us" />
+
+                </div>
+
+                <div > <img src="./images/svg1.svg" alt='' className='w-screen lg:max-w-xl' /></div>
+            </div>
+
+
+        </div>
+
+
+
+
+    );
+}
+export default land;
