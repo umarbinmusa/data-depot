b367e1c8781c871691ccc90c87e76ab0796f970d
oniboy6775 <onisabiabdullahi@gmail.com>
2023-10-05 06:21:46 +0100

    morgan package added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const helmet = require("helmet");
 const xss = require("xss-clean");
 const cors = require("cors");
 const path = require("path");
+const morgan = require("morgan");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(xss());
 app.use(cors());
+app.use(morgan("dev"));
 // app.use(express.static("client/build"));
 
 app.get("/", (req, res) => {
