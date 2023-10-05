bc739274f0f5d17c73142f7e5bb96c0894c0e1ec
oniboy6775 <onisabiabdullahi@gmail.com>
2023-10-05 09:36:09 +0100

    all structure added

---
+++ controllers/authController.js
@@ -0,0 +1,7 @@
+const login = (req, res) => {
+  res.send("login");
+};
+
+module.exports = {
+  login,
+};
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+// modules import
 const express = require("express");
-const ConnectDB = require("./utils/Connect");
 const helmet = require("helmet");
 const xss = require("xss-clean");
 const cors = require("cors");
 const path = require("path");
 const morgan = require("morgan");
-
+const colors = require("colors");
 const app = express();
-const PORT = process.env.PORT || 5000;
+// file imports
+const ConnectDB = require("./utils/Connect");
+
 // middleware
 require("dotenv").config();
 app.use(express.json());
@@ -15,8 +17,11 @@ app.use(helmet());
 app.use(xss());
 app.use(cors());
 app.use(morgan("dev"));
-// app.use(express.static("client/build"));
+app.use(express.static(__dirname + "/client/build"));
 
+// routes
+app.use("/api/v1/auth", () => {});
+const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Welcome to data depot");
 });
@@ -24,14 +29,14 @@ const start = async () => {
   try {
     await ConnectDB(process.env.MONGODB_URI);
     app.listen(PORT, () => {
-      console.log(`Connected to DB and Server is running on port ${PORT}`);
+      console.log(
+        `Connected to DB and Server is running on port ${PORT}`.yellow
+      );
     });
   } catch (err) {
     console.log(err);
   }
 };
-
-app.use(express.static(__dirname + "/client/build"));
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
